b2a08a9a06c57c944eeee64fdd5f3a3304247b2f
maybephonker <70798545+maybephonker@users.noreply.github.com>
2023-05-07 23:23:35 +0300

    Update spamsg.py

--- spamsg.py
+++ spamsg.py
@@ -24,7 +24,7 @@ async def spamg(e):
     else:
         await e.edit("Invalid command format, please reply to a gif to spam.")
 
-@register(outgoing=True, pattern="^.spams(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^.spams(?: |$)(.*)")
 async def spams(e):
     reply_msg = await e.get_reply_message()
     if reply_msg and reply_msg.sticker:
@@ -36,16 +36,13 @@ async def spams(e):
         if count <= 0:
             await e.edit("Invalid command format, please provide a positive number of stickers to spam.")
             return
-        if not reply_msg.sticker.set_id:
-            await e.edit("Invalid command format, please reply to a sticker from a sticker set to spam.")
-            return
-        sticker_set = await e.client(GetStickerSetRequest(InputStickerSetID(reply_msg.sticker.set_id)))
-        is_animated = isinstance(reply_msg.sticker, reply_msg.sticker.TGSSticker)
-        file = sticker_set.documents[reply_msg.sticker.documents[0].id - 1].document if not is_animated else reply_msg.sticker
         await e.delete()
         for i in range(count):
             await asyncio.sleep(0.2)  # add a 0.2 second delay between each message
-            await e.respond(file=file)
+            if reply_msg.sticker.is_animated:
+                await e.respond(file=reply_msg.sticker, reply_to=reply_msg, supports_streaming=True)
+            else:
+                await e.respond(file=reply_msg.sticker, reply_to=reply_msg)
     else:
         await e.edit("Invalid command format, please reply to a sticker to spam.")
 
