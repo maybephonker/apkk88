e92d65a640a52b40d619321a8675c4f5177ce84e
maybephonker <70798545+maybephonker@users.noreply.github.com>
2023-05-08 00:01:40 +0300

    Update spamsg.py

--- spamsg.py
+++ spamsg.py
@@ -1,48 +1,58 @@
 import asyncio
 from telethon.tl.types import InputStickerSetID, InputStickerSetShortName
 from telethon.tl.functions.messages import GetStickerSetRequest
-from telethon.errors.rpcerrorlist import BadRequestError
-from telethon import events, functions, types
-from userbot import bot, CMD_HELP
+from userbot import CMD_HELP
 from userbot.events import register
 
+@register(outgoing=True, pattern="^.spams")
+async def spam_sticker(event):
+    if not event.is_reply:
+        return await event.edit("Please reply to a sticker to spam.")
 
-@register(outgoing=True, pattern=r"^.spams(?: |$)(.*)")
-async def spams(e):
-    reply_msg = await e.get_reply_message()
-    if reply_msg and reply_msg.sticker:
-        try:
-            count = int(e.pattern_match.group(1).strip())
-        except ValueError:
-            await e.edit("Invalid command format, please provide a number of stickers to spam.")
-            return
-        if count <= 0:
-            await e.edit("Invalid command format, please provide a positive number of stickers to spam.")
-            return
-        await e.delete()
-        for i in range(count):
-            await asyncio.sleep(0.2)  # add a 0.2 second delay between each message
-            await e.respond(file=reply_msg.sticker, reply_to=reply_msg, supports_streaming=True)
-    elif reply_msg and reply_msg.gif:
-        try:
-            count = int(e.pattern_match.group(1).strip())
-        except ValueError:
-            await e.edit("Invalid command format, please provide a number of gifs to spam.")
-            return
-        if count <= 0:
-            await e.edit("Invalid command format, please provide a positive number of gifs to spam.")
-            return
-        await e.delete()
-        for i in range(count):
-            await asyncio.sleep(0.2)  # add a 0.2 second delay between each message
-            await e.respond(file=reply_msg.gif, reply_to=reply_msg)
-    else:
-        await e.edit("Invalid command format, please reply to a sticker or gif to spam.")
+    reply_msg = await event.get_reply_message()
+    if not reply_msg.sticker:
+        return await event.edit("Invalid command format, please reply to a sticker to spam.")
 
+    stickerset_attr = reply_msg.sticker.attributes[1]
+    stickerset = await event.client(
+        GetStickerSetRequest(
+            stickerset=InputStickerSetShortName(
+                stickerset_attr.short_name
+            )
+        )
+    )
+
+    while True:
+        await event.client.send_file(event.chat_id, reply_msg.sticker)
+        await asyncio.sleep(0.2)
+
+@register(outgoing=True, pattern="^.spamg")
+async def spam_gif(event):
+    if not event.is_reply:
+        return await event.edit("Please reply to a GIF to spam.")
+
+    reply_msg = await event.get_reply_message()
+    if not reply_msg.document or not reply_msg.document.mime_type.startswith("video/"):
+        return await event.edit("Invalid command format, please reply to a GIF to spam.")
+
+    stickerset_attr = reply_msg.document.attributes[1]
+    stickerset = await event.client(
+        GetStickerSetRequest(
+            stickerset=InputStickerSetID(
+                id=stickerset_attr.stickerset.id,
+                access_hash=stickerset_attr.stickerset.access_hash
+            )
+        )
+    )
+
+    while True:
+        await event.client.send_file(event.chat_id, reply_msg.document)
+        await asyncio.sleep(0.2)
 
 CMD_HELP.update({
-    "spams": ".spams <number of messages>\
-    \nUsage: Spams the replied sticker a specified number of times.",
-    "spamg": ".spamg <number of messages>\
-    \nUsage: Spams the replied gif a specified number of times."
+    "spamsticker":
+    ".spams\
+\nUsage: Reply to a sticker with .spams to spam it in the chat.\
+\n\n.spamg\
+\nUsage: Reply to a GIF with .spamg to spam it in the chat."
 })
