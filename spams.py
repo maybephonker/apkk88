1069ca551cd876ac2d8dd181fe3739b72b9d66ff
maybephonker <70798545+maybephonker@users.noreply.github.com>
2023-05-07 22:50:27 +0300

    Update spams.py

--- spams.py
+++ spams.py
@@ -22,9 +22,32 @@ async def spams(e):
     else:
         await e.edit("Invalid command format, please reply to a sticker to spam.")
 
+@register(outgoing=True, pattern="^\.spam(?: |$)(.*)")
+async def spam(e):
+    reply_msg = await e.get_reply_message()
+    if reply_msg and reply_msg.sticker:
+        try:
+            count = int(e.pattern_match.group(1).strip())
+        except ValueError:
+            await e.edit("Invalid command format, please provide a number of stickers to spam.")
+            return
+        if count <= 0:
+            await e.edit("Invalid command format, please provide a positive number of stickers to spam.")
+            return
+        await e.delete()
+        for i in range(count):
+            await asyncio.sleep(0.2)  # add a 0.2 second delay between each message
+            await e.respond(file=reply_msg)
+    else:
+        await e.edit("Invalid command format, please reply to a sticker to spam.")
 
 CMD_HELP.update({
     "spams":
     ".spams <number of messages>\
+    Usage: Spams the replied sticker a specified number of times."
+})
+CMD_HELP.update({
+    "spam":
+    ".spam <number of messages>\
     Usage: Spams the replied sticker a specified number of times."
 })
