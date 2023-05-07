3419fd6413fe70381519ac76f2820fe3db2bcc85
maybephonker <70798545+maybephonker@users.noreply.github.com>
2023-05-07 22:33:22 +0300

    Update ftgspam.py

--- ftgspam.py
+++ ftgspam.py
@@ -6,7 +6,7 @@ from userbot.events import register
 @register(outgoing=True, pattern="^.spams(?: |$)(.*)")
 async def spams(e):
     reply_msg = await e.get_reply_message()
-    if reply_msg and (reply_msg.media or reply_msg.gif):
+    if reply_msg and (reply_msg.sticker or reply_msg.gif):
         try:
             count = int(e.pattern_match.group(1).strip())
         except ValueError:
@@ -18,7 +18,7 @@ async def spams(e):
         await e.delete()
         media = reply_msg.media or reply_msg.gif
         for i in range(count):
-            await asyncio.sleep(1)  # add a 1 second delay between each message
+            await asyncio.sleep(0.2)  # add a 1 second delay between each message
             await e.respond(file=media)
     else:
         await e.edit("Invalid command format, please reply to a sticker or gif to spam.")
